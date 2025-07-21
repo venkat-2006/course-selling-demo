0d134418dc3a22261a7079f050d83acb5625706d
VENKAT6912 <vibecoder18@gmail.com>
2025-07-21 23:12:45 +0530

    admin's side done

--- routes/admin.js
+++ routes/admin.js
@@ -1,67 +1,67 @@
 
-const {Router}=require("express");
-const adminRouter=Router();
-const {adminModel}=require("../db");
-const jwt=require("jsonwebtoken"); //bcrypt,zod,jsonwebtoken
+const { Router } = require("express");
+const adminRouter = Router();
+const { adminModel, courseModel } = require("../db");
+const jwt = require("jsonwebtoken"); //bcrypt,zod,jsonwebtoken
 
-const{JWT_ADMIN_PASSWORD}=require("../config");
-const{adminMiddleware}=require("../middleware/admin");
+const { JWT_ADMIN_PASSWORD } = require("../config");
+const { adminMiddleware } = require("../middleware/admin");
 
 // adminRouter.use(adminMiddleWare)
 
-adminRouter.post("/signup",async function (req, res) {
-    const{email,password,firstName,lastName}=req.body;
-     // use zod validation (hashing password)
-     //hash password so plaintext is not stored in db
+adminRouter.post("/signup", async function (req, res) {
+    const { email, password, firstName, lastName } = req.body;
+    // use zod validation (hashing password)
+    //hash password so plaintext is not stored in db
 
-     await adminModel.create({//put inside try catch block
-        email:email,
-        password:password,
-        firstName:firstName,
-        lastName:lastName
-     })
+    await adminModel.create({//put inside try catch block
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+    })
 
     res.json({
         message: "signup succeeded"
     })
 
-    
+
 })
 
 adminRouter.post("/signin", async function (req, res) {
-    const{email,password}=req.body;
-  //ideally password should be hashed so u cant compare user provided password and db password
-    const admin=await adminModel.findOne({
-        email:email,
-        password:password
+    const { email, password } = req.body;
+    //ideally password should be hashed so u cant compare user provided password and db password
+    const admin = await adminModel.findOne({
+        email: email,
+        password: password
     });
-    if(admin){
-        const token=jwt.sign({
-            id:admin._id
-        },JWT_ADMIN_PASSWORD); //do cookie logic if u can
+    if (admin) {
+        const token = jwt.sign({
+            id: admin._id
+        }, JWT_ADMIN_PASSWORD); //do cookie logic if u can
         res.json({
-            token:token
+            token: token
         })
-    }else{
+    } else {
         res.status(403).json({
-            message:"incorrect credentials"
+            message: "incorrect credentials"
         })
 
     }
-    
+
 })
 
-adminRouter.post("/course", adminMiddleware,async function (req, res) {
-    const adminId=req.userId;
+adminRouter.post("/course", adminMiddleware, async function (req, res) {
+    const adminId = req.userId;
 
-    const{title,description,imageUrl,price}=req.body;
+    const { title, description, imageUrl, price } = req.body;
 
-    await courseModel.create({
-       title: title,
-       description:description,
-       imageUrl:imageUrl,
-       price:price,
-       adminId:adminId
+    const course = await courseModel.create({
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+        price: price,
+        creatorId: adminId
     })
 
 
@@ -69,20 +69,70 @@ adminRouter.post("/course", adminMiddleware,async function (req, res) {
 
     res.json({
         message: "course created",
-        courseId:course._id
+        courseId: course._id
 
     })
 })
-adminRouter.put("/course", function (req, res) {
-    res.json({
-        message: "course creation endpoint"
+adminRouter.put("/course", adminMiddleware, async function (req, res) {
+    const adminId = req.userId;
+
+    const { title, description, imageUrl, price, courseId } = req.body;
+
+    // const course=await courseModel.findOne({
+    //     _id:courseId,
+    //     creatorId:adminId
+    // })
+
+
+
+    const course = await courseModel.updateOne({
+        _id: courseId,
+        creatorId: adminId
+    }, {
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+        price: price,
+
     })
-})
-adminRouter.get("/course/bulk", function (req, res) {
+
+
+
+
     res.json({
-        message: "bulk course display endpoint"
+        message: "course updated",
+        courseId: course._id
+
+    })
+
+})
+adminRouter.get("/course/bulk", async function (req, res) {
+    const adminId = req.userId;
+
+
+    const courses = await courseModel.find({
+
+        creatorId: adminId
+    }
+
+        //,{
+        //    title: title,
+        //    description:description,
+        //    imageUrl:imageUrl,
+        //    price:price,
+
+        // } 
+    )
+
+
+
+
+    res.json({
+        message: "course updated",
+        courseId: course._id
+
     })
 })
 module.exports = {
-    adminRouter:adminRouter
+    adminRouter: adminRouter
 }
